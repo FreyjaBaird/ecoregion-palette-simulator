8737ed2d3c27428aa0063b5a0d62dae98bc36009
FreyjaBaird <freyjabaird@outlook.com>
2026-09-08 23:52:32 +0100

    Fix search URL for Wikipedia API

--- app.js
+++ app.js
@@ -265,7 +265,7 @@ async function getWikipediaCoords(cityName, region) {
     var suffix = (region === "UK") ? " United Kingdom" : " China";
     var strictQuery = prefix + cityName + suffix;
     
-    var searchUrl = "https://wikipedia.org" + 
+    var searchUrl = "https://en.wikipedia.org/w/api.php?action=query&generator=search&gsrsearch=" + 
                     encodeURIComponent(strictQuery) + 
                     "&gsrlimit=1&prop=coordinates&format=json&origin=*";
     
