52864def61b74f2f2f4a9f0bcab66020e015d9b9
FreyjaBaird <freyjabaird@outlook.com>
2026-09-14 00:20:54 +0100

    Fix syntax error in rainValues mapping function

--- app.js
+++ app.js
@@ -397,7 +397,7 @@ const rainMatches = [
 const rainValues =
   rainMatches
     .slice(0,12)
-    .map(m) => parseFloat(m[1]);
+    .map(m => parseFloat(m[1]));
 
 const annualRain =
   rainValues.reduce(
